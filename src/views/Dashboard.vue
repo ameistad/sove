4c38047e40fb32fe174fecc1aaabdca5d3ab9a85
ameistad <ameistad@gmail.com>
2018-04-26 19:03:12 +0200

    Add dashb

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
-    <button @click="deleteAllEntries" class="btn">Slett alle registreringer</button>
+    <button @click="showConfirmDeleteAllEntries = true" class="btn">Slett alle registreringer</button>
+
   </div>
 </template>
 
 <script>
+import Modal from '@/components/Modal'
+
 export default {
   name: 'Dashboard',
+  data () {
+    return {
+      showConfirmDeleteAllEntries: false
+    }
+  },
   methods: {
     deleteAllEntries () {
       this.$store.dispatch('deleteAllEntries')
     }
+  },
+  components: {
+    Modal
   }
 }
 </script>
